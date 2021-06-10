ec36d2a5e545cc3ac6a86adb08187c863d25cecb
mrcs22 <marcusmoraes2010@hotmail.com>
2021-06-10 18:05:38 -0300

    Creating endpoint to post a new participant

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import { stripHtml } from "string-strip-html";
+import dayjs from "dayjs";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const data = JSON.parse(fs.readFileSync("./src/data.json"));
+const participants = data.participants;
+const messages = data.messages;
+
+app.post("/participants", (req, res) => {
+  const now = dayjs().format("HH:mm:ss");
+  const name = stripHtml(req.body.name).result;
+
+  if (req.body.name === "") {
+    res.sendStatus(400);
+    return;
+  }
+
+  const participant = {
+    name,
+    lastStatus: Date.now(),
+  };
+  participants.push(participant);
+
+  const message = {
+    from: name,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: now,
+  };
+  messages.push(message);
+
+  fs.writeFileSync("./src/data.json", JSON.stringify(data));
+
+  res.sendStatus(200);
+});
+
+app.listen(4000);
